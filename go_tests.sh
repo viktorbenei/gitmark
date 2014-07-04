fdb91d357e33d36d3e69c2781d4ba8c4bd699c6b
Viktor Benei <viktor.benei@gmail.com>
2014-07-05 01:59:46 +0200

    go_tests : uses a .coverprofile, ignored

--- go_tests.sh
+++ go_tests.sh
@@ -3,6 +3,6 @@
 IFS=$'\n'
 for adir in $(find . -type d -maxdepth 1 -not -path '*/\.*' -not -name '.')
 do
-	(cd "$adir" && go test -coverprofile fmt)
+	(cd "$adir" && go test -coverprofile .coverprofile)
 done
 unset IFS
