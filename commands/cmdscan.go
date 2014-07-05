ae01eea912bac369ad34866cc60ed71c452d643a
Viktor Benei <viktor.benei@gmail.com>
2014-07-05 03:04:24 +0200

    Scan: non saving mode finished. Finds .git folders and respects scan-ignore patterns. In verbose mode prints ignores

--- commands/cmdscan.go
+++ commands/cmdscan.go
@@ -1,14 +1,82 @@
 package commands
 
 import (
-// "fmt"
-// "github.com/viktorbenei/gitmark/config"
-// "os/exec"
+	"errors"
+	"fmt"
+	"github.com/viktorbenei/gitmark/config"
+	"os"
+	"path/filepath"
+	"regexp"
 )
 
 var cmdScan = &Command{
 	Usage: "scan",
 	Short: "scan for local repositories which are not yet bookmarked",
-	Run:   nil,
+	Run:   runScan,
 	Name:  "scan",
+}
+
+var (
+	isStore       bool
+	scanRoot      string
+	isScanVerbose bool
+	scanIgnores   []string
+)
+
+func init() {
+	cmdScan.Flag.StringVar(&scanRoot, "rootpath", "", "[required] root path of scan")
+	cmdScan.Flag.BoolVar(&isStore, "store", false, "store found repositories in bookmarks?")
+	cmdScan.Flag.BoolVar(&isScanVerbose, "verbose", false, "verbose?")
+}
+
+func runScan(cmd *Command, args []string) error {
+	if isScanVerbose {
+		fmt.Println("scanning...")
+		fmt.Println(" (i) Using ignores:")
+		scanIgnores = config.GitmarkConfig.ScanIgnores
+		fmt.Println(scanIgnores)
+	}
+
+	if scanRoot == "" {
+		return errors.New("No scan rootpath provided")
+	}
+
+	err := filepath.Walk(scanRoot, func(path string, fi os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Println(err) // can't walk here,
+			return nil       // but continue walking elsewhere
+		}
+		if !fi.IsDir() {
+			return nil // not a dir.  ignore.
+		}
+		matched, err := filepath.Match("*.git", fi.Name())
+		if err != nil {
+			fmt.Println(err) // malformed pattern
+			return err       // this is fatal.
+		}
+		if matched {
+			isPathIsMatch := true
+			if scanIgnores != nil && len(scanIgnores) > 0 {
+				for _, aScanIgnore := range scanIgnores {
+					matchedIgnore, err := regexp.MatchString(aScanIgnore, path)
+					if err != nil {
+						fmt.Println(" (!)", err) // malformed pattern
+					}
+					if matchedIgnore {
+						if isScanVerbose {
+							fmt.Println(" -> ignore match:", path, "|", aScanIgnore)
+						}
+						isPathIsMatch = false
+						break
+					}
+				}
+			}
+			if isPathIsMatch {
+				fmt.Println(path)
+			}
+		}
+		return nil
+	})
+
+	return err
 }
