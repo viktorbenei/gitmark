ef500530b60f3f780c0f991f5176b249957e71af
Viktor Benei <viktor.benei@gmail.com>
2014-07-05 02:22:52 +0200

    go_tests : go test ./… comment

--- go_tests.sh
+++ go_tests.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#
+# without code coverage profiling you can simply do
+#     $ go test ./...
+# to test all packages, but if you want to run coverage tests you have to call go test 
+#  in every package folder separately
+#
+
 IFS=$'\n'
 for adir in $(find . -type d -maxdepth 1 -not -path '*/\.*' -not -name '.')
 do
