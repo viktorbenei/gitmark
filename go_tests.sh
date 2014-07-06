a235b2f1ed6d6fb0997ccd329488c8b895bacaf9
Viktor Benei <viktor.benei@gmail.com>
2014-07-06 19:51:28 +0200

    go_tests : will print if a test failed and will exit with non-zero code if at least one test fails - for CI and automations

--- go_tests.sh
+++ go_tests.sh
@@ -7,9 +7,20 @@
 #  in every package folder separately
 #
 
+is_test_failed=0
 IFS=$'\n'
 for adir in $(find . -type d -maxdepth 1 -not -path '*/\.*' -not -name '.')
 do
 	(cd "$adir" && go test -coverprofile .coverprofile)
+	if [ $? -ne 0 ]; then
+		is_test_failed=1
+	fi
 done
 unset IFS
+
+if [ $is_test_failed -ne 0 ]; then
+	echo " [!] At least one of the tests failed!"
+	exit 1
+fi
+
+echo " [OK] Tests finished with success"
