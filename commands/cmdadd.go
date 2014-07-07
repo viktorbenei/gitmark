872d2125a5f7b2677becefdd4812249aa813f311
Viktor Benei <viktor.benei@gmail.com>
2014-07-07 23:07:37 +0200

    add command : implemented

--- commands/cmdadd.go
+++ commands/cmdadd.go
@@ -1,8 +1,14 @@
 package commands
 
 import (
-// "fmt"
-// "github.com/viktorbenei/gitmark/config"
+	"bufio"
+	"errors"
+	"fmt"
+	"github.com/viktorbenei/gitmark/config"
+	"github.com/viktorbenei/gitmark/pathutil"
+	"os"
+	"os/exec"
+	"strings"
 )
 
 var cmdAdd = &Command{
@@ -12,6 +18,140 @@ var cmdAdd = &Command{
 	Name:  "add",
 }
 
+func generateTitleFromRepositoryUrl(repositoryUrl string) (string, error) {
+	trimmedUrl := strings.TrimSpace(repositoryUrl)
+	if trimmedUrl == "" {
+		return "", errors.New("Invalid repository url: empty url")
+	}
+	repoUrlCompontents := strings.Split(trimmedUrl, "/")
+	if len(repoUrlCompontents) < 1 {
+		return "", errors.New("")
+	}
+	extensionComps := strings.Split(repoUrlCompontents[len(repoUrlCompontents)-1], ".")
+	return extensionComps[0], nil
+}
+
+func generateLocalPathFromUserInputPathAndRepositoryUrl(userInputPath, repositoryUrl string) (string, error) {
+	titleFromUrl, err := generateTitleFromRepositoryUrl(repositoryUrl)
+	if err != nil {
+		return "", err
+	}
+
+	var localPath string
+	if userInputPath[len(userInputPath)-1:] == "/" {
+		localPath = userInputPath + titleFromUrl
+	} else {
+		localPath = userInputPath
+	}
+	return localPath, nil
+}
+
+func cloneRepository(repositoryUrl string, r config.Repository) error {
+	fmt.Println(" (i) Cloning...")
+	c := exec.Command("git", "clone", repositoryUrl, r.Path)
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
+	if err := c.Start(); err != nil {
+		return err
+	}
+	if err := c.Wait(); err != nil {
+		return err
+	}
+	return nil
+}
+
 func runAdd(cmd *Command, args []string) error {
+	fmt.Println(" (i) Adding new repository")
+
+	fmt.Printf("-> Repository url: ")
+	bio := bufio.NewReader(os.Stdin)
+	lineBytes, hasMoreInLine, err := bio.ReadLine()
+	if err != nil {
+		return err
+	}
+	if hasMoreInLine {
+		return errors.New("Input too long!")
+	}
+	repoUrl := fmt.Sprintf("%s", lineBytes)
+	if len(repoUrl) < 1 {
+		return errors.New("URL can't be empty!")
+	}
+
+	defaultTitle, err := generateTitleFromRepositoryUrl(repoUrl)
+	if err != nil {
+		fmt.Println("Can't generate a default title for the url")
+	}
+	fmt.Printf("-> Bookmark Title [%s]: ", defaultTitle)
+	bio = bufio.NewReader(os.Stdin)
+	lineBytes, hasMoreInLine, err = bio.ReadLine()
+	if err != nil {
+		return err
+	}
+	if hasMoreInLine {
+		return errors.New("Input too long!")
+	}
+	repoTitle := fmt.Sprintf("%s", lineBytes)
+	if repoTitle == "" {
+		// use default
+		repoTitle = defaultTitle
+	}
+
+	fmt.Printf("-> Local Path: ")
+	bio = bufio.NewReader(os.Stdin)
+	lineBytes, hasMoreInLine, err = bio.ReadLine()
+	if err != nil {
+		return err
+	}
+	if hasMoreInLine {
+		return errors.New("Input too long!")
+	}
+	repoLocalPath := fmt.Sprintf("%s", lineBytes)
+	if len(repoLocalPath) < 1 {
+		return errors.New("Path can't be empty!")
+	}
+	repoLocalPath = pathutil.ExpandPath(repoLocalPath)
+	if repoLocalPath == "" {
+		return errors.New("Failed to expand the provided path.")
+	}
+	repoLocalPath, err = generateLocalPathFromUserInputPathAndRepositoryUrl(repoLocalPath, repoUrl)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("--- Adding:")
+	fmt.Println("Repository.URL:", repoUrl)
+	fmt.Println("Repository.Title:", repoTitle)
+	fmt.Println("Repository.LocalPath:", repoLocalPath)
+	fmt.Println("-----------")
+
+	// check whether the folder is a local working copy of the repository
+	isCloneNeeded := false
+	expectedGitDirPath := fmt.Sprintf("%s/.git", repoLocalPath)
+	fmt.Println(" (i) Checking for .git at", expectedGitDirPath)
+	if _, err := os.Stat(expectedGitDirPath); err != nil {
+		if os.IsNotExist(err) {
+			isCloneNeeded = true
+		}
+	}
+
+	r := config.Repository{Title: repoTitle, Path: repoLocalPath}
+	if isCloneNeeded {
+		if err := cloneRepository(repoUrl, r); err != nil {
+			return err
+		}
+	} else {
+		fmt.Println(" (i) Repository found locally - no clone needed")
+	}
+
+	fmt.Println(" (i) Storing into Bookmarks...")
+	if err := config.GitmarkConfig.AddRepository(r); err != nil {
+		return err
+	}
+	if err := config.WriteGitmarkConfigToFile(); err != nil {
+		return err
+	}
+
+	fmt.Println(" (i) Added to gitmarks")
+
 	return nil
 }
--- commands/cmdadd_test.go
+++ commands/cmdadd_test.go
@@ -13,11 +13,78 @@ func TestCmdAddExistence(t *testing.T) {
 	}
 }
 
-func Test_runAdd(t *testing.T) {
-	t.Log("should run")
+func Test_generateTitleFromRepositoryUrl(t *testing.T) {
+	t.Log("should return the last component of the url, without any extension (.git, .ext, ...)")
 
-	err := runAdd(nil, nil)
+	repoTitle, err := generateTitleFromRepositoryUrl("")
+	if err == nil {
+		t.Error("Should return an error for empty url")
+	}
+	repoTitle, err = generateTitleFromRepositoryUrl(" ")
+	if err == nil {
+		t.Error("Should return an error for a whitespace url")
+	}
+
+	repoTitle, err = generateTitleFromRepositoryUrl("a")
 	if err != nil {
-		t.Error("should run without errors")
+		t.Error("Should NOT return an error for a non empty url")
+	}
+	if repoTitle != "a" {
+		t.Error("Not the expected title. Got:", repoTitle)
+	}
+
+	repoTitle, err = generateTitleFromRepositoryUrl("a/b.git")
+	if err != nil {
+		t.Error("Should NOT return an error for a non empty url")
+	}
+	if repoTitle != "b" {
+		t.Error("Not the expected title. Got:", repoTitle)
+	}
+
+	repoTitle, err = generateTitleFromRepositoryUrl("https://github.com/viktorbenei/gitmark.git")
+	if err != nil {
+		t.Error("Should NOT return an error for a non empty url")
+	}
+	if repoTitle != "gitmark" {
+		t.Error("Not the expected title. Got:", repoTitle)
+	}
+
+	repoTitle, err = generateTitleFromRepositoryUrl("git@github.com:viktorbenei/gitmark.git")
+	if err != nil {
+		t.Error("Should NOT return an error for a non empty url")
+	}
+	if repoTitle != "gitmark" {
+		t.Error("Not the expected title. Got:", repoTitle)
 	}
 }
+
+func Test_generateLocalPathFromUserInputPathAndRepositoryUrl(t *testing.T) {
+	t.Log("should return the same path if it doesn't end with a slash, or append the repo url's last component if it ends with /")
+
+	testRepoUrl := "git@github.com:viktorbenei/gitmark.git"
+
+	genLocalPath, err := generateLocalPathFromUserInputPathAndRepositoryUrl("/some/path", testRepoUrl)
+	if err != nil {
+		t.Error("Should not return an error for a valid path and URL")
+	}
+	if genLocalPath != "/some/path" {
+		t.Error("Path without a trailing / should generate the same. Got:", genLocalPath)
+	}
+
+	genLocalPath, err = generateLocalPathFromUserInputPathAndRepositoryUrl("/some/path/", testRepoUrl)
+	if err != nil {
+		t.Error("Should not return an error for a valid path and URL")
+	}
+	if genLocalPath != "/some/path/gitmark" {
+		t.Error("Path with a trailing / should append the repo URL's last component. Got:", genLocalPath)
+	}
+}
+
+// func Test_runAdd(t *testing.T) {
+// 	t.Log("should run")
+
+// 	err := runAdd(nil, nil)
+// 	if err != nil {
+// 		t.Error("should run without errors")
+// 	}
+// }
